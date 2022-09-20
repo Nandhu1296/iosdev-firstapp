7c5924431891dfcc43ad33d691a52a5066f72086
Access-Lab <wsuaccesslab@gmail.com>
2022-09-20 12:57:08 -0500

    commit by nandha

--- iosdev-firstapp/ViewController.swift
+++ iosdev-firstapp/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeImage: UIImageView!
+    let imageCollection = [UIImage(imageLiteralResourceName: "ball2"), UIImage(imageLiteralResourceName: "ball3"), UIImage(imageLiteralResourceName: "ball4"), UIImage(imageLiteralResourceName: "ball1"),UIImage(imageLiteralResourceName: "ball5")]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clickButton(_ sender: Any) {
+        changeImage.image = imageCollection.randomElement()
+    }
+    
 }
 
